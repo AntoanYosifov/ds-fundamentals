b343584d021c9a32d56730a1157359d8508806e4
Antoan Yosifov <134812071+AntoanYosifov@users.noreply.github.com>
2026-07-19 11:57:22 +0200

    BalancedParentheses solution

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,10 @@
 import implementations.DoublyLinkedList;
+import implementations.Queue;
+import implementations.Stack;
 
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-
-        ArrayList<Integer> integers = new ArrayList<>();
-
-        integers.add(1);
-        integers.add(2);
-        integers.add(3);
-
-        String i = "1";
-
-        boolean contains = integers.contains(i);
     }
 }
--- src/main/java/implementations/BalancedParentheses.java
+++ src/main/java/implementations/BalancedParentheses.java
@@ -5,12 +5,43 @@ import interfaces.Solvable;
 public class BalancedParentheses implements Solvable {
     private String parentheses;
 
+    private Stack<Character> openersStack;
+
     public BalancedParentheses(String parentheses) {
         this.parentheses = parentheses;
+        this.openersStack = new Stack<>();
     }
 
     @Override
     public Boolean solve() {
-        return null;
+
+        for (int i = 0; i < parentheses.length(); i++) {
+            char c = parentheses.charAt(i);
+
+            switch (c) {
+                case '{':
+                case '[':
+                case '(':
+                    openersStack.push(c);
+                    break;
+                case ')':
+                    if (openersStack.isEmpty() || openersStack.pop() != '(') {
+                        return false;
+                    }
+                    break;
+                case ']':
+                    if (openersStack.isEmpty() || openersStack.pop() != '[') {
+                        return false;
+                    }
+                    break;
+                case '}':
+                    if (openersStack.isEmpty() || openersStack.pop() != '{') {
+                        return false;
+                    }
+                    break;
+            }
+        }
+
+        return openersStack.isEmpty();
     }
 }
